54ebded25b6b2a93ad6eab60feea6b5dda8688f8
rons003 <ronelle003@gmail.com>
2018-12-04 14:44:23 +0800

    Upload Code

---
+++ ASP_CR/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP_CR.Startup))]
+namespace ASP_CR
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
